ae8fac3e1c8e5e0a170177e3ceb44fb5bac2a921
lakindulk <lakindulk9@gmail.com>
2023-03-12 12:00:31 +0530

    third  commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as BRouter, Route, Routes } from "react-router-dom";
 import Signin from "./Screen/signin";
 import Profilepage from "./Screen/profilepage";
+import Studentexam from "./Screen/studentexam";
+import Teacherexam from "./Screen/teacherexam";
 
 function App() {
   return (
@@ -9,8 +11,10 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Signin />} />
         <Route exact path="/profile" element={<Profilepage />} />
+        <Route exact path="/studentexam" element={<Studentexam />} />
+        <Route exact path="/teacherexam" element={<Teacherexam />} />
 
-
+        
       </Routes>
     </BRouter>
   );
--- src/Screen/profilepage.js
+++ src/Screen/profilepage.js
@@ -23,9 +23,9 @@ export default function profilepage() {
                     <div style={{ paddingLeft: '2vh', paddingTop: '2vh' }}>
                         <p style={{ fontSize: '30px' }}><Image src="https://play-lh.googleusercontent.com/7Ac5TgaL15Ra4bvFVHJKCdJp4qvnL4djZj5bKc6RN-MZjzrvkeHbJytek0NPTSdZcp8" style={{ width: '12%' }} roundedCircle={true} />
                             &nbsp; John Perea</p>
-                        <div style={{ paddingTop: '2vh', paddingLeft: '20vh', paddingRight: '30vh' }}>
+                        <div style={{ paddingTop: '2vh', paddingLeft: '25vh', paddingRight: '35vh' }}>
 
-                            <Card style={{ backgroundColor: '#330A6A' }}>
+                            <Card style={{ backgroundColor: '#330A6A', borderRadius: '18px' }}>
                                 <div style={{ paddingLeft: '2vh', paddingTop: '2vh' }}>
                                     <h5 style={{ color: 'white' }}>Change password</h5>
                                     <p style={{ color: '#8FE5FF' }}>Changing the login password frequently prevents unauthorized users from modifying configurations</p>
@@ -41,7 +41,7 @@ export default function profilepage() {
                                                 <div style={{ paddingRight: '15vh' }}>
                                                     <Form.Group className="mb-3" controlId="formBasicEmail">
 
-                                                        <Form.Control style={{ backgroundColor: '#D8B7FA' }} type="password" placeholder="Enter password" />
+                                                        <Form.Control style={{ backgroundColor: '#D8B7FA', borderRadius: '18px' }} type="password" placeholder="Enter password" />
                                                     </Form.Group>
                                                 </div>
                                             </Col>
@@ -54,7 +54,7 @@ export default function profilepage() {
                                                 <div style={{ paddingRight: '15vh' }}>
                                                     <Form.Group className="mb-3" controlId="formBasicEmail">
 
-                                                        <Form.Control style={{ backgroundColor: '#D8B7FA' }} type="password" placeholder="Enter New Password" />
+                                                        <Form.Control style={{ backgroundColor: '#D8B7FA', borderRadius: '18px' }} type="password" placeholder="Enter New Password" />
                                                     </Form.Group>
                                                 </div>
                                             </Col>
@@ -66,15 +66,15 @@ export default function profilepage() {
                                                 <div style={{ paddingRight: '15vh' }}>
                                                     <Form.Group className="mb-3" controlId="formBasicEmail">
 
-                                                        <Form.Control style={{ backgroundColor: '#D8B7FA' }} type="password" placeholder="Confirm Password" />
+                                                        <Form.Control style={{ backgroundColor: '#D8B7FA', borderRadius: '18px' }} type="password" placeholder="Confirm Password" />
                                                     </Form.Group>
                                                 </div>
                                             </Col>
                                         </Row>
 
 
-                                        <div style={{ paddingLeft:'40%',paddingTop:'10vh'}}>
-                                            <Button style={{backgroundColor: '#8FE5FF',color:'black',paddingLeft:'7vh',paddingRight:'7vh'}} variant="primary" size="lm">
+                                        <div style={{ paddingLeft: '40%', paddingTop: '8vh' }}>
+                                            <Button style={{ backgroundColor: '#8FE5FF', color: 'black', paddingLeft: '7vh', paddingRight: '7vh', borderRadius: '18px' }} variant="primary" size="lm">
                                                 ~ Save ~
                                             </Button>
                                         </div>
